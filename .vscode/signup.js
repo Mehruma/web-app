9114ce7e4580541630ee1ed4c872bd2f01d281b1
musammath shababa <shababam@lsbu.ac.uk>
2024-11-04 14:48:26 +0000

    Merged files from teammate's folder into main web app

--- .vscode/signup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    // You can add code here to validate the form, send data to the server, etc.
-
-    // For now, we'll just redirect to the employee page
-    window.location.href = 'employee.html'; // Redirect to employee page after signup
-});
